f906f57cf64c6f8a35c6f6c7fa6d5cff93ac371d
rafal <r.p.wojciechowski10@gmail.com>
2024-04-10 21:11:59 +0200

    added password hasher to registration

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -10,11 +10,12 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
     #[Route('/api/register', name: 'app_register')]
-    public function index(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $users = $entityManager->getRepository(User::class);
 
@@ -26,34 +27,40 @@ class RegisterController extends AbstractController
         
         if ($checkedUser) {
             $rawResponse['response'] = 'user istnieje';
+
         } else {
+
             if ($content->password == $content->passwordRepeated) {
+
                 if (strlen($content->password) > 6) {
-                    if (preg_match('#[0-9]#',$content->password) && preg_match('"/[\[^\'£$%^&*()}{@:\'#~?><>,;@\|\\\-=\-_+\-¬\`\]]/',$content->password)) {
+
+                    if (preg_match('#[0-9]#',$content->password) && preg_match('/[\'\/~`\!@#\$%\^&\*\(\)_\-\+=\{\}\[\]\|;:"\<\>,\.\?\\\]/',$content->password)) {
                         $newUser = new User();
                         $newUser->setEmail($dataEmail);
-                        $newUser->setPassword($content->password);
+
+                        $hashedPassword = $passwordHasher->hashPassword(
+                            $newUser,
+                            $content->password
+                        );
+                        $newUser->setPassword($hashedPassword);
+
                         $entityManager->persist($newUser);
                         $entityManager->flush();
+
                         $rawResponse['response'] = 'jest ok';
+
                     } else {
                         $rawResponse['response'] = 'haslo musi zawierać przynajmniej jedną cyfrę i znak specjalny'; 
                     }
-                    
 
                 } else {
                     $rawResponse['response'] = 'hasło jest za krtótkie'; 
                 }
 
-                  
-
-
-
             } else {
                 $rawResponse['response'] = 'hasla nie sa takie same';
             }
         
-
         }
 
         $response = json_encode($rawResponse);
